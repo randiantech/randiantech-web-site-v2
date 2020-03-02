54edb7f40bd313732a5a354eb62a9640b5524a7b
Juan Carlos Cancela <cancela.juancarlos@gmail.com>
2020-03-01 22:09:18 -0300

    implemented blogs section

--- src/components/BlogGroup/BlogGroup.tsx
+++ src/components/BlogGroup/BlogGroup.tsx
@@ -16,39 +16,39 @@ const isMobileWidth = () => window.innerWidth <= MOBILE_WIDTH;
 const ITEMS = [
   {},
   {
-    title: 'Some Title 2',
+    title: 'Using BPF to Transform SSH Sessions into Structured Events',
     description:
-      'fkfjdklf fkdsjfklsdjf jfkdlsjfklsdjfkl jfdksjfsdklf jfklsdjfsdkf jfksldjf',
+      `Teleport 4.2 introduced a new feature called Enhanced Session Recording that takes an unstructured SSH session and outputs a stream of structured events. It’s the next step in Teleport’s evolution that uses new technology (eBPF or now simply known as BPF) to close some gaps in Teleport’s audit abilities. Below you can see an illustration of this feature and if you keep reading, we’ll get into some of the technical details.`,
     img: '/blog/img1.jpg',
-    url: 'Some URL',
+    url: 'https://findwork.dev/blog/advanced-usage-python-requests-timeouts-retries-hooks/',
   },
   {
-    title: 'Some Title 3',
+    title: 'Iranian Hackers Have Been ‘Password-Spraying’ the US Grid',
     description:
-      'fkfjdklf fkdsjfklsdjf jfkdlsjfklsdjfkl jfdksjfsdklf jfklsdjfsdkf jfksldjf',
+      'In the wake of the US assassination of Iranian general Qasem Soleimani and the retaliatory missile strike that followed, Iran-watchers have warned that the country could deploy cyberattacks as well, perhaps even targeting US critical infrastructure like the electric grid. A new report lends some fresh details to the nature of that threat: By all appearances, Iranian hackers dont currently have the capability to start causing blackouts in the US. But they’ve been working to gain access to American electric utilities, long before tensions between the two countries came to a head.',
     img: '/blog/img2.jpg',
     url: 'Some URL',
   },
   {
-    title: 'Some Title 4',
+    title: 'Ford CEO says the company "overestimated" self-driving cars',
     description:
-      'fkfjdklf fkdsjfklsdjf jfkdlsjfklsdjfkl jfdksjfsdklf jfklsdjfsdkf jfksldjf',
+      "Ford CEO Jim Hackett scaled back hopes about the company's plans for self-driving cars this week, admitting that the first vehicles will have limits. 'We overestimated the arrival of autonomous vehicles,' said Hackett, who once headed the company's autonomous vehicle division, at a Detroit Economic Club event on Tuesday. While Ford still plans on launching its self-driving car fleet in 2021, Hackett added that 'its applications will be narrow, what we call geo-fenced, because the problem is so complex'.",
     img: '/blog/img3.jpg',
     url: 'Some URL',
   },
   {
-    title: 'Some Title 5',
+    title: 'Epistemic standards for “Why did it take so long to invent X?”',
     description:
-      'fkfjdklf fkdsjfklsdjf jfkdlsjfklsdjfkl jfdksjfsdklf jfklsdjfsdkf jfksldjf',
+      'In seeking to understand the history of progress, I keep running across intriguing cases of “ideas behind their time”—inventions that seem to have come along much later than they could have, such as the cotton gin or the bicycle. I’ve started collecting a list here, and will update that page with new analyses as I find them.',
     img: '/blog/img4.jpg',
-    url: 'Some URL',
+    url: 'https://rootsofprogress.org/epistemic-standards-for-why-it-took-so-long',
   },
   {
-    title: 'Some Title 6',
+    title: 'Advanced usage of Python requests - timeouts, retries, hooks',
     description:
-      'fkfjdklf fkdsjfklsdjf jfkdlsjfklsdjfkl jfdksjfsdklf jfklsdjfsdkf jfksldjf',
+      'The Python HTTP library requests is probably my favourite HTTP utility in all the languages I program in. Its simple, intuitive and ubiquitous in the Python community. Most of the programs that interface with HTTP use either requests or urllib3 from the standard library.',
     img: '/blog/img5.jpg',
-    url: 'Some URL',
+    url: 'https://findwork.dev/blog/advanced-usage-python-requests-timeouts-retries-hooks/',
   }
 ];
 
@@ -76,25 +76,26 @@ export default class BlogGroup extends React.Component<any, BlogGroupState> {
         className={`blog-group-container-${isMobile ? 'mobile' : 'desktop'}`}
       >
         {ITEMS.map((item, index) => {
+          const {url, title, description, img} = item;
           if (index === 0) {
             return (
               <div className="blog-group-first-item">News and Articles</div>
             );
           }
           return (
-            <div className="blog-group-item-desktop">
+            <a className="blog-group-item-desktop" href={url}>
               <div>
-                <img src={item.img} className="blog-group-item-img-desktop" />
+                <img src={img} className="blog-group-item-img-desktop" />
               </div>
-              <div>
+              <div className="blog-group-item-details">
                 <div className="blog-group-item-title-desktop">
-                  {item.title}
+                  {title}
                 </div>
                 <div className="blog-group-item-description-desktop">
-                  {item.description}
+                  {description}
                 </div>
               </div>
-            </div>
+            </a>
           );
         })}
       </div>
