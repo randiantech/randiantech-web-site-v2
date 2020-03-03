e82e423f3c7ee105143104e169a282a7d4ba9d27
Juan Carlos Cancela <cancela.juancarlos@gmail.com>
2020-03-03 00:00:26 -0300

    added form validation

--- src/components/ContactUs/ContactUs.tsx
+++ src/components/ContactUs/ContactUs.tsx
@@ -40,7 +40,10 @@ export default class ContactUs extends React.Component<any, any> {
     super(props);
     this.state = {
       isMobile: isMobileWidth(),
-      error: false,
+      errorFirstName: false,
+      errorLastName: false,
+      errorEmail: false,
+      errorPhone: false,
       firstName: '',
       lastName: '',
       phone: '',
@@ -51,6 +54,10 @@ export default class ContactUs extends React.Component<any, any> {
   componentDidMount() {
     window.addEventListener('resize', this.resize.bind(this));
     this.resize();
+    this.setState({errorFirstName: ''});
+    this.setState({errorLastName: ''});
+    this.setState({errorPhone: ''});
+    this.setState({errorEmail: ''});
   }
 
   resize() {
@@ -59,41 +66,76 @@ export default class ContactUs extends React.Component<any, any> {
 
   validateInput() {
     const {firstName, lastName, phone, email} = this.state;
-    const isValidPhoneNumber = new RegExp(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im);
-    const isValidEmail = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+    const isValidPhoneNumber = new RegExp(
+      /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+    );
+    const isValidEmail = new RegExp(
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    );
 
-    const isValidInput = firstName.length > 1 && lastName.length > 1 && isValidPhoneNumber.test(phone) && isValidEmail.test(email);
-
-    if(!isValidInput) {
-      return this.setState({error: true});
-    }
-    return this.setState({error: false});
+    this.setState({errorFirstName: firstName.length < 1});
+    this.setState({errorLastName: lastName.length < 1});
+    this.setState({errorPhone: !isValidPhoneNumber.test(phone)});
+    this.setState({errorEmail: !isValidEmail.test(email)});
   }
 
   render() {
-    const {isMobile, firstName, lastName, phone, email, error} = this.state;
+    const {
+      isMobile,
+      firstName,
+      lastName,
+      phone,
+      email,
+      errorFirstName,
+      errorLastName,
+      errorPhone,
+      errorEmail,
+    } = this.state;
     return (
       <div className="contact-us-container">
         <div className="contact-us-container-title">Contact Us</div>
         <div className="contact-us-container-form">
           {ITEMS.map(item => (
-            <div className="contact-us-item">
-              <div className="contact-us-item-label">{item.label}</div>
+            <div key={`key-${item.label}`} className="contact-us-item">
+              <div className="contact-us-item-label">
+                <div>{item.label}</div>
+                {this.state[
+                  `error${item.id.charAt(0).toUpperCase() +
+                    item.id.substring(1)}`
+                ] && (
+                  <div className="contact-us-item-label-error-container">
+                    <img
+                      src="./alert-circle.png"
+                      className="contact-us-item-label-error"
+                    />
+                    <span className="contact-us-item-label-error-label">
+                      Required
+                    </span>
+                  </div>
+                )}
+              </div>
               <input
+                defaultValue=""
                 className="contact-us-item-input"
                 type="text"
                 name={`randiantech-${item.id}`}
-                onChange={e => {this.setState({[item.id]: e.target.value}); console.log(this.state)}}
+                onChange={e => {
+                  this.setState({[item.id]: e.target.value});
+                  this.validateInput();
+                }}
+                onFocus={() => this.validateInput()}
               />
             </div>
           ))}
         </div>
         <div className="contact-us-send-btn-container">
-          <div className="contact-us-send-btn" onClick={() => this.validateInput()}>Send</div>
+          <div
+            className="contact-us-send-btn"
+            onClick={() => this.validateInput()}
+          >
+            Send
+          </div>
         </div>
-        {
-          error && <div>ERROR!!!</div>
-        }
       </div>
     );
   }
