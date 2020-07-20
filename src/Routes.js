9abd6c77243a23b1765920ae62acf3f7ecfce28e
juancancela <cancela.juancarlos@gmail.com>
2020-07-19 21:04:12 -0300

    implemented contact section

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import App from "./App";
 import { HomePage } from "./pages/HomePage";
@@ -11,21 +11,28 @@ import { ContactPage } from "./pages/ContactPage";
 import { RTSPage } from "./pages/RTSPage";
 import { AppContextProvider } from "./AppContext";
 
-const Routes = () => (
-  <AppContextProvider>
-    <App>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/about" component={AboutPage} />
-        <Route exact path="/services" component={ServicesPage} />
-        <Route exact path="/team" component={TeamPage} />
-        <Route exact path="/careers" component={CareersPage} />
-        <Route exact path="/clients" component={ClientsPage} />
-        <Route exact path="/contact" component={ContactPage} />
-        <Route exact path="/rts/login" component={RTSPage} />
-      </Switch>
-    </App>
-  </AppContextProvider>
-);
+const Routes = () => {
+  useEffect(() => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }, []);
+
+  return (
+    <AppContextProvider>
+      <App>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/services" component={ServicesPage} />
+          <Route exact path="/team" component={TeamPage} />
+          <Route exact path="/careers" component={CareersPage} />
+          <Route exact path="/clients" component={ClientsPage} />
+          <Route exact path="/contact" component={ContactPage} />
+          <Route exact path="/rts/login" component={RTSPage} />
+        </Switch>
+      </App>
+    </AppContextProvider>
+  );
+};
 
 export default Routes;
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,8 +1,53 @@
 import React from "react";
+import styled from "styled-components";
+import { color, size, dist } from "../theme";
 import { ContactUsSection } from "../sections/ContactUsSection/ContactUsSection";
+import { ImageGallerySection } from "../sections/ImageGallerySection";
+
+const headerImages = [
+  {
+    original: "/images/contact-us-3.png",
+  },
+];
+
+const Image = styled.div`
+  border-bottom: 1px solid ${color.defAppColor};
+`;
+
+export const Label = styled.div`
+  position: absolute;
+  top: 50%;
+  left: calc(100% - 500px);
+  width: 300px;
+  color: white;
+  font-weight: bolder;
+  background: var(--main-app-color);
+  padding: ${dist.defItemDistance};
+  border-radius: 50px;
+  border-bottom-left-radius: 0;
+  cursor: pointer;
+  z-index: 1;
+
+  .label {
+    font-size: 25px;
+    text-align: center;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 20px 20px 60px var(--main-deg-app-color),
+      -20px -20px 60px var(--main-app-color);
+  }
+`;
 
 export const ContactPage = React.memo(() => (
   <div>
+    <Image>
+      <Label>
+        <div className="label">We are Ready.</div>
+      </Label>
+      <ImageGallerySection images={headerImages} />
+    </Image>
     <ContactUsSection isMobile={false} />
   </div>
 ));
